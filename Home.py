fec0957c07711c49e737cfdc05d39b79fe6da406
auqa5325 <auqa5325@gmail.com>
2026-02-03 14:59:10 +0530

    Update page title for the application

--- Home.py
+++ Home.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()  
-st.set_page_config(page_title="AUQA Questify", page_icon="📘", layout="wide")
+st.set_page_config(page_title="TANIII Automated Question Paper Generation", page_icon="📘", layout="wide")
 
 st.title("📘 AUQA Questify")
 st.markdown("""
 Welcome to **AUQA Questify** 🚀
 
 Use the sidebar to navigate:
-- **Ingestion** → Upload course PDFs from S3, extract with Textract, embed with Titan, and store in OpenSearch.  
-- **Generation** → Search ingested docs, apply hybrid retrieval, and generate practice questions using Anthropic, Llama, or Mistral models.  
 """)
